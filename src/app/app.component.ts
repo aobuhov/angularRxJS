691bbbdb76dbfe48812f06d0dcf6ed419460bab1
Aleksei Obukhov <alexey.obuhov@gmail.com>
2023-05-07 16:47:00 +0300

    refactor to effects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {Store} from "@ngrx/store";
-import {clear, countSelector, decrease, increase} from "./reducers/counter";
+import {clear, countSelector, decrease, increase, updatedAtSelector} from "./reducers/counter";
 import {map} from "rxjs";
 
 @Component({
@@ -9,24 +9,22 @@ import {map} from "rxjs";
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  undatedAt?: number;
   count$ = this.store.select(countSelector);
   cannotDecrease$ = this.count$.pipe(map(cnt => cnt<=0));
+  updatedAt$ = this.store.select(updatedAtSelector);
+
   constructor(private store: Store) {
 
   }
 
   increase(): void {
-    this.undatedAt = Date.now();
     this.store.dispatch(increase());
   }
   decrease(): void {
-    this.undatedAt = Date.now();
     this.store.dispatch(decrease());
   }
 
   clear(): void {
-    this.undatedAt = Date.now();
     this.store.dispatch(clear());
   }
 
