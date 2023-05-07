4d2d2d693e60372fe82267cd570246f4a0dd4df5
Aleksei Obukhov <alexey.obuhov@gmail.com>
2023-05-07 15:59:38 +0300

    install:  - ng add @ngrx/store@latest --no-minimal  - ng add @ngrx/store-devtools@latest  - ng add @ngrx/effects@latest --no-minimal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { AppEffects } from './app.effects';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    EffectsModule.forRoot([AppEffects])
+    //StoreModule.forRoot({}, {})
   ],
   providers: [],
   bootstrap: [AppComponent]
